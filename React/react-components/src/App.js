ea5afb2839f49a1ccb8ea74218909fab0902acc1
Matt Sullivan <mattsully01@gmail.com>
2025-01-21 11:18:25 -0700

    adding react-components for future react component boilerplates

---
+++ React/react-components/src/App.js
@@ -0,0 +1,12 @@
+import Spinner from './components/Spinner';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Spinner />
+    </div>
+  );
+}
+
+export default App;
