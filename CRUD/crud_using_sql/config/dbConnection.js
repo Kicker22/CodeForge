4c5e41c0a8c29390872719c95a55ff3afa10616b
Matt Sullivan <mattsully01@gmail.com>
2025-01-27 19:24:38 -0700

    adding new crud with sql, still working on getting it to work properly

---
+++ CRUD/crud_using_sql/config/dbConnection.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+  host: process.env.host,
+  port: process.env.port,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Successfully connected to the database');
+});
+
+module.exports = connection;
